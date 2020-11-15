3effe14c295a3e92245face81fbf1cf130f3e86c
slightly-nerdy <github@slightlynerdy.org>
2020-11-15 13:43:01 +0000

    Basic grid input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Startingwords = [
+  'a1', 'a2', 'a3', 'a4',
+  'b1', 'b2', 'b3', 'b4',
+  'c1', 'c2', 'c3', 'c4',
+  'd1', 'd2', 'd3', 'd4',
+];
 
+
+// function XApp() {
+
+//   const [words] = useState(Xwords);
+  
+
+//   function handleInputChange(event: any) {
+//     const value = event.target.value;
+//     const name = event.target.name;
+
+//     console.log(event, value, name);
+//   }
+
+//   return (
+//     <div className="wall">
+//       {words.map((word, index) => (
+//         <TextInput
+//           style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+//           onChangeText={text => onChangeText(text)}
+//           value={value}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+
+  type AppState = {words: Array<string>}
+class App extends React.Component<any, AppState> {
+  constructor(props : any) {
+    super(props);
+    this.state = {words: Startingwords};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event: any) {
+    const {name, value} = event.target;
+    const index = parseInt(name);
+
+    let newWords = [...this.state.words];
+    newWords[index] = value;
+    this.setState({words: newWords});
+  }
+
+  render() {
+    return (
+      <div className="wall">
+        {this.state.words.map((word, index) => (
+          <input type="text" 
+            value={word} 
+            key={index.toString()}
+            name={index.toString()}
+            onChange={this.handleChange} />
+         ))}
+      </div>
+    );
+  }
+}
 export default App;
