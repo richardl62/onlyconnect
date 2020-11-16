777a049cf137180365736e6082a9bb1def513b80
slightly-nerdy <github@slightlynerdy.org>
2020-11-16 11:54:33 +0000

    Seperate 'Enter' and 'Guess' stages

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,86 @@ import React, { FC, useState} from 'react';
 import { Clue } from './clue';
 import './App.css';
 
-const startingWords = [
-  'a1', 'a2', 'a3', 'a4',
+// Shuffle array in place. Return the shuffled array
+function shuffleArray<T>(array: Array<T>) {
+  // From https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+const startingClues = [
+  'a1.1', 'a2', 'a3', 'a4',
   'b1', 'b2', 'b3', 'b4',
   'c1', 'c2', 'c3', 'c4',
   'longlonglong', 'd2', 'd3', 'd4',
 ];
 
+const startingState = startingClues.map((clue: string, index: number) => {
+  return {
+    clue: clue,
+    group: index / 4,
+  };
+});
 
+type State = typeof startingState[0];
+
+interface InputWallProps {
+  state: Array<State>, 
+  clueChange?: (index: number, clue: string) => void, 
+  clueSelected?: (index: number) => void, 
+};
+
+const Wall: FC<InputWallProps> = ({ state, clueChange, clueSelected}: InputWallProps) => {
+  return (
+    <div className="wall">
+      {state.map((s: State, index: number) => (
+        <Clue key={index.toString()} clue={s.clue} index={index} 
+          onChange={clueChange}
+          onSelect={clueSelected}
+          />
+      ))}
+    </div>
+  );
+}
 
 interface AppProps {};
 const App: FC<AppProps> = () => {
 
-    const [clues, setClues] = useState(startingWords);
+    const [state, setState] = useState(startingState);
+    const [wordsEntered, setWordsEntered] = useState(false);
 
+
+    
+
+  if (!wordsEntered) {
     const clueChange: (index: number, newClue: string) => void = (index, newClue) => {
-      let newClues = [...clues];
-      newClues[index] = newClue;
-      setClues(newClues);
+      let newState = [...state];
+      newState[index] = {...newState[index], clue:newClue};
+      setState(newState);
     }
 
-    return (
-      <div className="wall">
-        {clues.map((clue, index) => (
-            <div key={index.toString()}> 
-              <Clue clue={clue} index={index} onChange={clueChange} />
-            </div> 
-         ))}
-      </div>
+    const finishedEnteringWords: () => void = () => {
+      setState(shuffleArray([...state]));
+      setWordsEntered(true);
+    }
+
+    return (<>
+      <div>Enter clues then press 'Done'</div>
+      <Wall state={state} clueChange={clueChange} />
+      <button type="button" onClick={finishedEnteringWords}>Done</button>
+    </>
     );
+  } else {
+
+    const clueSelected: (index: number) => void = (index) => {
+      console.log(`clue selected :`, index);
+    }
+
+    return <Wall state={state} clueSelected={clueSelected}/>
+  }
 
 }
 export default App;
--- src/clue.tsx
+++ src/clue.tsx
@@ -4,16 +4,28 @@ import React, { FC } from 'react';
 interface ClueProps {
     clue: string;
     index: number;
-    onChange: (index: number, word: string) => void;
+    onChange?: (index: number, word: string) => void;
+    onSelect?: (index:number) => void;
 }
 
-const Clue : FC<ClueProps> = ({clue, index, onChange}: ClueProps) => {
+const Clue : FC<ClueProps> = ({clue, index, onChange, onSelect}: ClueProps) => {
   
-    const onClueChange: (event: any) => void = (event) => {
-        onChange(index, event.target.value)
-    };
+    if (onChange) {
+        const onClueChange: (event: any) => void = (event) => {
+            onChange(index, event.target.value)
+        };
 
-  return (<input type="text" value={clue} onChange={onClueChange}/>);
+        return (<input type="text" value={clue} onChange={onClueChange} />);
+    }
+    else if (onSelect) {
+        const onClick: () => void = () => {
+            onSelect(index)
+        };
+        return (<div onClick={onClick}>{clue}</div>);
+    }
+    else {
+        throw Error("Oops");
+    }
 }
 
 export { Clue };
