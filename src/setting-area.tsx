c49990c65b7f4ad2ee36a6d8fe6474d64c73ee9b
slightly-nerdy <github@slightlynerdy.org>
2021-02-20 15:23:09 +0000

    Add  'clear stored solutions'  button

--- src/setting-area.tsx
+++ src/setting-area.tsx
@@ -1,14 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { clearAllStorage } from './url-and-storage-tools'
 
 function trimmed(words: Array<string>) {
     console.log("Pre-trimmed", words);
 
-    let result : Array<string> = [];
-    for(let i in words) {
+    let result: Array<string> = [];
+    for (let i in words) {
         const t = words[i].trim();
         if (t) {
             result.push(t);
-        } 
+        }
     }
 
     return result;
@@ -26,8 +27,7 @@ function getCluesCommaSeperated(text: string) {
 interface SettingAreaProps {
     recordClues: (clues: Array<string> | null) => void;
 }
-function SettingArea({recordClues} : SettingAreaProps)
-{
+function SettingArea({ recordClues }: SettingAreaProps) {
     const [recordedText, setRecordedText] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
 
@@ -41,46 +41,52 @@ function SettingArea({recordClues} : SettingAreaProps)
 
         const spaceSeparated = getCluesSpaceSeperated(recordedText);
         console.log("Space separated", spaceSeparated);
-        if(spaceSeparated.length === 16) {
+        if (spaceSeparated.length === 16) {
             recordClues(spaceSeparated);
             return;
-        } 
-        
+        }
+
         const commaSeparated = getCluesCommaSeperated(recordedText);
         console.log("Comma separated", commaSeparated);
 
-        if(commaSeparated.length === 16) {
+        if (commaSeparated.length === 16) {
             recordClues(commaSeparated);
             return;
-        } 
+        }
 
         setErrorMessage(`Did not find 16 clues: 
             ${spaceSeparated.length} found with space seperation.
-            ${commaSeparated.length} found with comma-seperation.`     
+            ${commaSeparated.length} found with comma-seperation.`
         );
 
-        
+
     }
 
     const ErrorMessage = () => {
-        if(errorMessage) {
+        if (errorMessage) {
             return <div>{"Error: " + errorMessage}</div>
         }
         return null;
     }
-    
-    return (<>
-        <div>
+
+    return (
+        <div className="setting-area" >
             <p>Enter clues.  These can be single words, or words and phrases seperated by commas</p>
-            <textarea  
+            <textarea
                 cols={50}
                 rows={4}
                 onChange={textAreaChange}
             />
+
+            <div className="setting-area-buttons">
+                <button className="done-button" onClick={onDone}>Done</button>
+                <button className="clear-stored-solutions-button" onClick={clearAllStorage}>
+                    Clear stored solutions
+            </button>
+            </div>
+            <ErrorMessage />
         </div>
-        <button className="done-button" onClick={onDone}>Done</button>
-        <ErrorMessage/>
-    </>);
+    );
 }
 
 export default SettingArea;
